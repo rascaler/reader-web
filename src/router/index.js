ca5270e2ca29a5410bdee52778bae32fe705164d
rascaler <rascaler@163.com>
2017-10-12 22:13:57 +0800

    [添加]muse-ui测试

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,8 @@
 import Vue from 'vue'
 import Router from 'vue-router'
 import HelloWorld from '@/components/HelloWorld'
+import museui from '@/pages/museui'
+import 'muse-ui/dist/muse-ui.css'
 
 Vue.use(Router)
 
@@ -10,6 +12,10 @@ export default new Router({
       path: '/',
       name: 'Hello',
       component: HelloWorld
+    }, {
+      path: '/museui',
+      name: 'museui',
+      component: museui
     }
   ]
 })
